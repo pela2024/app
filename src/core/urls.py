17695dd0ad4ea98bf08ed61941e004301626141a
Gresia Cristian Gustavo <gustavogresia22@gmail.com>
2025-02-08 18:37:33 -0300

    feat: enrutamiento de core y productos

---
+++ src/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+
+app_name = "core"
+
+urlpatterns = [
+    path("core/", views.index, name="index"),
+]
---
+++ src/productos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+
+app_name = "productos"
+
+urlpatterns = [
+    path("productos/", views.index, name="index"),
+]
