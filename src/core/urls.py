88fd01129b99c5a4fc93a34210a55bc245ed808c
_Gustavo Gresia <gustavogresia22@gmail.com>
2025-02-16 20:18:10 -0300

    feat: creacion de index y about

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
+from .views import index, about
 
 
 
 app_name = "core"
 
 urlpatterns = [
-    path("core/", views.index, name="index"),
+    path("", index, name="index"),
+    path("about/", about, name="about"),
 ]
